3a235b40fadcba276670dc0863f9700375e15c46
Ben Coombes <benzcoombes@gmail.com>
2022-12-13 17:36:52 -0500

    removed unused py file

--- main.py
+++
@@ -1,6 +0,0 @@
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client() #instantiates a client
-client.connect('localhost', 1883) #connects to the server
-client.publish('COMP216', payload='Hello from Narendra')
-client.disconnect()
