9a9cd6ad2114a0fc00bda468d2120075670be200
Ben Coombes <benzcoombes@gmail.com>
2022-12-13 19:01:45 -0500

    publisher started

--- publisher.py
+++ publisher.py
@@ -5,27 +5,39 @@
 import time
 import paho.mqtt.client as mqtt
 import json
+import numpy as np
 
 
 class Publisher:
-    my_data = {
-        'Time': time.asctime(),
-        'x': 0,
-        'Stock Price': 10,
-    }
+    def __init__(self, start_price, volatility, trend, stock_name):
+        self.stock_data = {
+            'Time': time.asctime(),
+            'Stock Price': start_price,
+            'Stock Name': stock_name
+        }
+        self.last_price = start_price
+        self.volatility = volatility
+        self.trend = trend
 
     def start_client(self):
         client = mqtt.Client()
         client.connect('localhost', 1883)
         client.loop_start()
+        client.publish('STOCKS/' + self.stock_data['Stock Name'], json.dumps(self.stock_data))
         time.sleep(1)
-        for x in range(10):
-            self.my_data['Time'] = time.asctime()
-            client.publish('COMP216/test', json.dumps(self.my_data))
-            print('Message sent')
+        while True:
             time.sleep(15)
-        client.loop_stop()
+            self.stock_data['Time'] = time.asctime()
+            self.stock_data['Stock Price'] = self.generate_stock_price()
+            client.publish('STOCKS/' + self.stock_data['Stock Name'], json.dumps(self.stock_data))
+            print(self.stock_data['Stock Price'])
+
+    def generate_stock_price(self):
+        price = self.last_price + self.trend + (1 + np.random.normal(0, self.volatility))
+        self.last_price = price
+        return price
 
 
-pub = Publisher()
+pub = Publisher(10, 5, 1, 'Tesla')
+
 pub.start_client()
